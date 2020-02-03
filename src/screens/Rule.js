b9358bd0a196a48c416139a932f91c9114c8ecd9
masaki_yamanaka <ki.ymnka9@gmail.com>
2020-02-03 16:51:38 +0900

    create rule screen

---
+++ src/screens/Rule.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import colors from "../styles/colors";
+import NumButton from "../components/buttons/NumButton";
+import RoundedButton from "../components/buttons/RoundedButton";
+import { globalStyles } from "../styles/global";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default class imageOutput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  render() {}
+}
+
+const styles = StyleSheet.create({});
